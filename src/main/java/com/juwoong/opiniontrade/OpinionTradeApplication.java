fbebe3988110ff9db43013edcc339bde878cf2b8
juwoong kim <wwong7247@gmail.com>
2024-01-23 19:50:38 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/juwoong/opiniontrade/OpinionTradeApplication.java
@@ -0,0 +1,13 @@
+package com.juwoong.opiniontrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinionTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juwoong/opiniontrade/OpinionTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwoong.opiniontrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
