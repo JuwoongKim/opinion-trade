360614ea812d8538a0cce8569f6c69b5a75e9e68
juwoong kim <wwong7247@gmail.com>
2024-01-25 12:02:01 +0900

    feat: ScrapProduct 도메인 객체 누락된 필드명 추가

--- src/main/java/com/juwoong/opiniontrade/user/domain/ScrapProduct.java
+++ src/main/java/com/juwoong/opiniontrade/user/domain/ScrapProduct.java
@@ -1,7 +1,16 @@
 package com.juwoong.opiniontrade.user.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class ScrapProduct {
+	@Column(name = "scrap_product_id", nullable = false)
+	private String scrapProductId;
+
+	@Column(name = "scrip_product_title", nullable = false)
+	private String scrapSurveyTitle;
+
+	@Column(name = "scrip_product_price", nullable = false)
+	private Long scrapSurveyPrice;
 }
