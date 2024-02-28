8d75237940df53ff9be7dece990810f3bd7ad9d8
juwoong kim <wwong7247@gmail.com>
2024-02-29 00:59:38 +0900

    Update: 설문결과 도메인 객체 정리

--- src/main/java/com/juwoong/opiniontrade/survey/domain/SurveyResult.java
+++ src/main/java/com/juwoong/opiniontrade/survey/domain/SurveyResult.java
@@ -13,29 +13,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "survey-results")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SurveyResult extends TimeBaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "survey_result_id")
 	private Long id;
 
-	@Getter
 	@Embedded
 	private Respondent respondent;
 
-	@Getter
 	@ElementCollection
 	@CollectionTable(name = "survey_result_answers", joinColumns = @JoinColumn(name = "survey_result_id"))
 	private List<Answer> answers;
 
-	protected SurveyResult() {
+	public static SurveyResult init(Respondent respondent, List<Answer> answers) {
+		return new SurveyResult(null, respondent, answers);
 	}
 
-	public SurveyResult(Respondent respondent, List<Answer> answers) {
-		this.respondent = respondent;
-		this.answers = answers;
+	public void createAnswer(Answer answer) {
+		answers.add(answer);
 	}
 }
