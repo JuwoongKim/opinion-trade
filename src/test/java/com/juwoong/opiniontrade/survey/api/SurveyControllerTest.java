69e3e3b1f5a140079a4666d37a89b89e651c94c0
juwoong kim <wwong7247@gmail.com>
2024-03-03 12:38:08 +0900

    Test: 설문지 수정에 대한 controller, service slice테스트 작성

--- src/test/java/com/juwoong/opiniontrade/survey/api/SurveyControllerTest.java
+++ src/test/java/com/juwoong/opiniontrade/survey/api/SurveyControllerTest.java
@@ -1,17 +1,21 @@
 package com.juwoong.opiniontrade.survey.api;
 
+import static com.juwoong.opiniontrade.global.exception.ErrorCode.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.juwoong.opiniontrade.global.exception.OpinionTradeException;
 import com.juwoong.opiniontrade.survey.api.request.SurveyRequest;
 import com.juwoong.opiniontrade.survey.application.SurveyService;
 import com.juwoong.opiniontrade.survey.application.response.SurveyResponse;
@@ -28,7 +32,8 @@ class SurveyControllerTest {
 	private ObjectMapper objectMapper;
 
 	@Test
-	void createSurvey_success() throws Exception {
+	@DisplayName("설문지 생성에 성공 시 201 상태코드, 설문지 식별자 응답값을 반환한다.")
+	void createSurvey_Success() throws Exception {
 		// given
 		Long creatorId = 1L;
 		String title = "surveyTitle";
@@ -53,7 +58,8 @@ class SurveyControllerTest {
 	}
 
 	@Test
-	void createSurvey_fail_withOutOfLength() throws Exception {
+	@DisplayName("사용자 입력값 오류로 인해 설문지 생성 실패 시 400 상태코드, BAD_REQUEST 메세지를 반환한다.")
+	void createSurvey_Fail_WithOutOfLength() throws Exception {
 		// given
 		Long creatorId = 1L;
 		String title = "0000000000/0000000000/0000000000/0000000000/0000000000/";
@@ -68,5 +74,47 @@ class SurveyControllerTest {
 
 		resultActions.andExpect(status().isBadRequest())
 			.andExpect(jsonPath("code").value("BAD_REQUEST"));
+	}
+
+	@Test
+	@DisplayName("설문지 수정 성공 시 204 상태코드와 응답값 없음을 반환한다.")
+	void updateSurvey_Success() throws Exception {
+		// given
+		Long surveyId = 1L;
+		String title = "surveyTitle";
+		String description = "surveyDescription";
+		SurveyRequest.Update request = new SurveyRequest.Update(title, description);
+
+		doNothing().when(surveyService).updateSurvey(surveyId, title, description);
+
+		// when then
+		ResultActions result = mockMvc.perform(put("/surveys/{id}", surveyId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))
+		);
+
+		result.andExpect(status().isNoContent());
+	}
+
+	@Test
+	@DisplayName("수정할 설문지가 존재하지 않으면 404 에러가 발생한다.")
+	void updateSurvey_Fail_When_No_Survey() throws Exception {
+		Long surveyId = 1L;
+		String title = "surveyTitle";
+		String description = "surveyDescription";
+		SurveyRequest.Update request = new SurveyRequest.Update(title, description);
+
+		doThrow(new OpinionTradeException(NOT_FOUND_SURVEY))
+			.when(surveyService).updateSurvey(eq(surveyId), eq(title), eq(description));
+
+		// when then
+		ResultActions result = mockMvc.perform(put("/surveys/{id}", surveyId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))
+		);
+
+		result.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.code").value(NOT_FOUND_SURVEY.getHttpStatus().name()))
+			.andExpect(jsonPath("$.message").value(NOT_FOUND_SURVEY.getMessage()));
 	}
 }
--- src/test/java/com/juwoong/opiniontrade/survey/application/SurveyServiceTest.java
+++ src/test/java/com/juwoong/opiniontrade/survey/application/SurveyServiceTest.java
@@ -1,8 +1,12 @@
 package com.juwoong.opiniontrade.survey.application;
 
+import static com.juwoong.opiniontrade.global.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.juwoong.opiniontrade.global.exception.OpinionTradeException;
 import com.juwoong.opiniontrade.survey.application.response.SurveyResponse;
 import com.juwoong.opiniontrade.survey.domain.Creator;
 import com.juwoong.opiniontrade.survey.domain.Survey;
@@ -24,6 +29,19 @@ class SurveyServiceTest {
 	@Mock
 	private SurveyRepository surveyRepository;
 
+	private Survey surveyFixture;
+
+	@BeforeEach
+	void setUp() {
+		Long creatorId = 1L;
+		String title = "surveyTitle";
+		String description = "surveyDescription";
+		Creator creator = Creator.init(creatorId);
+		SurveyInfo surveyInfo = SurveyInfo.init(title, description);
+
+		surveyFixture = Survey.init(creator, surveyInfo);
+	}
+
 	@Test
 	@DisplayName("유저가 설문지 생성에 성공한다.")
 	void createSurvey() {
@@ -31,11 +49,7 @@ class SurveyServiceTest {
 		Long creatorId = 1L;
 		String title = "surveyTitle";
 		String description = "surveyDescription";
-		Creator creator = Creator.init(creatorId);
-		SurveyInfo surveyInfo = SurveyInfo.init(title, description);
-
-		Survey survey = Survey.init(creator, surveyInfo);
-		when(surveyRepository.save(any())).thenReturn(survey);
+		when(surveyRepository.save(any())).thenReturn(surveyFixture);
 
 		// when
 		SurveyResponse surveyResponse = surveyService.createSurvey(creatorId, title, description);
@@ -43,4 +57,36 @@ class SurveyServiceTest {
 		// then
 		verify(surveyRepository, times(1)).save(any(Survey.class));
 	}
+
+	@Test
+	@DisplayName("설문지 정보 수정에 성공 한다.")
+	void updateSurvey_Succes_With_NoExceptions() {
+		// given
+		Long surveyId = 1L;
+		String title = "surveyTitle";
+		String description = "surveyDescription";
+		when(surveyRepository.findById(anyLong())).thenReturn(Optional.of(surveyFixture));
+
+		// when
+		surveyService.updateSurvey(surveyId, title, description);
+
+		// then
+		verify(surveyRepository, times(1)).findById(anyLong());
+	}
+
+	@Test
+	@DisplayName("설문지가 존재하지 않으면 예외가 발생한다.")
+	void updateSurvey_Fail_When_Not_Found_Survey() {
+		// given
+		Long surveyId = 1L;
+		String title = "surveyTitle";
+		String description = "surveyDescription";
+		when(surveyRepository.findById(anyLong())).thenThrow(new OpinionTradeException(NOT_FOUND_SURVEY));
+
+		// when then
+		assertThatExceptionOfType(OpinionTradeException.class)
+			.isThrownBy(()->surveyService.updateSurvey(surveyId, title, description))
+			.withMessage(NOT_FOUND_SURVEY.getMessage());
+	}
+
 }
