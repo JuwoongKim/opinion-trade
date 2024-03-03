0cd10a975ef251772d6c31fdf0d2df48983962dd
juwoong kim <wwong7247@gmail.com>
2024-03-03 10:56:46 +0900

    Chore: 수정이 필요한 유저 도메인 주석 처리

--- src/main/java/com/juwoong/opiniontrade/user/api/UserAccountController.java
+++ src/main/java/com/juwoong/opiniontrade/user/api/UserAccountController.java
@@ -1,44 +1,42 @@
-package com.juwoong.opiniontrade.user.api;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.juwoong.opiniontrade.user.api.request.LoginRequest;
-import com.juwoong.opiniontrade.user.api.request.SignUpRequest;
-import com.juwoong.opiniontrade.user.application.UserAccountService;
-import com.juwoong.opiniontrade.user.application.response.LoginResponse;
-import com.juwoong.opiniontrade.user.application.response.SignUpResponse;
-import com.juwoong.opiniontrade.user.domain.Email;
-import com.juwoong.opiniontrade.user.domain.Password;
-
-@RestController
-@RequestMapping("/users/account")
-public class UserAccountController {
-	private final UserAccountService userService;
-
-	public UserAccountController(UserAccountService userService) {
-		this.userService = userService;
-	}
-
-	@PostMapping("/sign-up")
-	public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest signUpRequest) {
-		Email email = signUpRequest.email();
-		Password password = signUpRequest.password();
-		SignUpResponse signUpResponse = userService.signUp(email, password);
-
-		return new ResponseEntity<>(signUpResponse, HttpStatus.CREATED);
-	}
-
-	@PostMapping("/login")
-	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-		Email email = loginRequest.email();
-		Password password = loginRequest.password();
-		LoginResponse loginResponse = userService.login(email, password);
-
-		return ResponseEntity.ok(loginResponse);
-	}
-}
+// package com.juwoong.opiniontrade.user.api;
+//
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import com.juwoong.opiniontrade.user.api.request.LoginRequest;
+// import com.juwoong.opiniontrade.user.api.request.SignUpRequest;
+// import com.juwoong.opiniontrade.user.application.UserAccountService;
+// import com.juwoong.opiniontrade.user.domain.Email;
+// import com.juwoong.opiniontrade.user.domain.Password;
+//
+// @RestController
+// @RequestMapping("/users/account")
+// public class UserAccountController {
+// 	private final UserAccountService userService;
+//
+// 	public UserAccountController(UserAccountService userService) {
+// 		this.userService = userService;
+// 	}
+//
+// 	@PostMapping("/sign-up")
+// 	public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest signUpRequest) {
+// 		Email email = signUpRequest.email();
+// 		Password password = signUpRequest.password();
+// 		SignUpResponse signUpResponse = userService.signUp(email, password);
+//
+// 		return new ResponseEntity<>(signUpResponse, HttpStatus.CREATED);
+// 	}
+//
+// 	@PostMapping("/login")
+// 	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+// 		Email email = loginRequest.email();
+// 		Password password = loginRequest.password();
+// 		LoginResponse loginResponse = userService.login(email, password);
+//
+// 		return ResponseEntity.ok(loginResponse);
+// 	}
+// }
--- src/main/java/com/juwoong/opiniontrade/user/application/UserAccountService.java
+++ src/main/java/com/juwoong/opiniontrade/user/application/UserAccountService.java
@@ -1,37 +1,37 @@
-package com.juwoong.opiniontrade.user.application;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.juwoong.opiniontrade.user.application.response.LoginResponse;
-import com.juwoong.opiniontrade.user.application.response.SignUpResponse;
-import com.juwoong.opiniontrade.user.domain.Email;
-import com.juwoong.opiniontrade.user.domain.Password;
-import com.juwoong.opiniontrade.user.domain.repository.UserRepository;
-
-@Service
-@Transactional(readOnly = true)
-public class UserAccountService {
-	private final UserRepository userRepository;
-
-	public UserAccountService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
-	@Transactional
-	public SignUpResponse signUp(Email email, Password password) {
-		// new User(email, password)
-		// save
-
-		Long tempUserId = 1L;
-		return new SignUpResponse(tempUserId);
-	}
-
-	public LoginResponse login(Email email, Password password) {
-		// findByEmailAndPassword(email)
-		// validatePassword(password)
-
-		Long tempUserId = 1L;
-		return new LoginResponse(tempUserId);
-	}
-}
+// package com.juwoong.opiniontrade.user.application;
+//
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
+//
+// import com.juwoong.opiniontrade.user.application.response.LoginResponse;
+// import com.juwoong.opiniontrade.user.application.response.SignUpResponse;
+// import com.juwoong.opiniontrade.user.domain.Email;
+// import com.juwoong.opiniontrade.user.domain.Password;
+// import com.juwoong.opiniontrade.user.domain.repository.UserRepository;
+//
+// @Service
+// @Transactional(readOnly = true)
+// public class UserAccountService {
+// 	private final UserRepository userRepository;
+//
+// 	public UserAccountService(UserRepository userRepository) {
+// 		this.userRepository = userRepository;
+// 	}
+//
+// 	@Transactional
+// 	public SignUpResponse signUp(Email email, Password password) {
+// 		// new User(email, password)
+// 		// save
+//
+// 		Long tempUserId = 1L;
+// 		return new SignUpResponse(tempUserId);
+// 	}
+//
+// 	public LoginResponse login(Email email, Password password) {
+// 		// findByEmailAndPassword(email)
+// 		// validatePassword(password)
+//
+// 		Long tempUserId = 1L;
+// 		return new LoginResponse(tempUserId);
+// 	}
+// }
