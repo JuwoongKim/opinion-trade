7f38f018af7f7c05842da99dde7fc4869cc6efe4
juwoong kim <wwong7247@gmail.com>
2024-03-19 20:19:16 +0900

    Test: 설문지 단일 조회 기능 테스트 코드 작성

--- src/test/java/com/juwoong/opiniontrade/survey/domain/repository/SurveyRepositoryTest.java
+++ src/test/java/com/juwoong/opiniontrade/survey/domain/repository/SurveyRepositoryTest.java
@@ -1,97 +1,54 @@
-// package com.juwoong.opiniontrade.survey.domain.repository;
-//
-// import static com.juwoong.opiniontrade.survey.domain.Question.Type.*;
-// import static org.assertj.core.api.AssertionsForClassTypes.*;
-//
-// import java.util.List;
-//
-// import org.assertj.core.api.Assertions;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.test.annotation.Rollback;
-//
-// import com.juwoong.opiniontrade.survey.domain.Creator;
-// import com.juwoong.opiniontrade.survey.domain.Option;
-// import com.juwoong.opiniontrade.survey.domain.Question;
-// import com.juwoong.opiniontrade.survey.domain.Survey;
-//
-// @DataJpaTest
-// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// class SurveyRepositoryTest {
-//
-// 	@Autowired
-// 	SurveyRepository surveyRepository;
-//
-// 	@Test
-// 	@DisplayName("설문지 생성 테스트")
-// 	void saveSurvey() {
-// 		// given
-// 		Creator creator = new Creator(1L, "juwoong");
-// 		Survey survey = new Survey(creator, "title", "description");
-//
-// 		// when
-// 		Survey createdSurvey = surveyRepository.save(survey);
-//
-// 		// then
-// 		assertThat(createdSurvey.getSurveyId()).isNotNull();
-// 	}
-//
-// 	@Test
-// 	@DisplayName("설문지 질문 동일 생명 주기 확인을 위한 질문 생성 테스트")
-// 	@Rollback(value = false)
-// 	void createSurveyQuestion() {
-// 		// given
-// 		// 설문지 생성
-// 		Creator creator = new Creator(1L, "juwoong");
-// 		Survey survey = new Survey(creator, "title", "description");
-//
-// 		// 질문 생성
-// 		List<Option> options = List.of(
-// 			new Option("Question_Option1"),
-// 			new Option("Question_Option1")
-// 		);
-// 		Question.Type type = MULTIPLE_CHOICE;
-// 		Question question = type.create("title", "description", options);
-//
-// 		Integer questionOrder = 1;
-//
-// 		survey.createQuestion(questionOrder, question);
-//
-// 		// when
-// 		Survey createdSurvey = surveyRepository.save(survey);
-//
-// 		// then
-// 		assertThat(createdSurvey.getSurveyId()).isNotNull();
-// 	}
-//
-// 	@Test
-// 	@DisplayName("설문지 질문 동일 생명 주기 확인을 위한 질문 삭제 테스트")
-// 	@Rollback(value = false)
-// 	void removeSurveyQuestion() {
-// 		// given
-// 		Creator creator = new Creator(1L, "juwoong");
-// 		Survey survey = new Survey(creator, "title", "description");
-//
-// 		List<Option> options = List.of(
-// 			new Option("Question_Option1"),
-// 			new Option("Question_Option1")
-// 		);
-// 		Question.Type type = MULTIPLE_CHOICE;
-// 		Question question = type.create("title", "description", options);
-//
-// 		Integer questionOrder = 1;
-//
-// 		survey.createQuestion(questionOrder, question);
-// 		Survey createdSurvey = surveyRepository.save(survey);
-//
-// 		// when then
-// 		Survey selectedSurvey = surveyRepository.findById(createdSurvey.getSurveyId()).orElseThrow();
-// 		surveyRepository.delete(selectedSurvey);
-//
-// 		Assertions.assertThatCode(() -> surveyRepository.delete(selectedSurvey))
-// 			.doesNotThrowAnyException();
-// 	}
-// }
+package com.juwoong.opiniontrade.survey.domain.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.juwoong.opiniontrade.survey.domain.Question;
+import com.juwoong.opiniontrade.survey.domain.Survey;
+import com.juwoong.opiniontrade.survey.fixture.QuestionFixture;
+import com.juwoong.opiniontrade.survey.fixture.SurveyFixture;
+
+import jakarta.persistence.EntityManager;
+
+@DataJpaTest
+	// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SurveyRepositoryTest {
+
+	@Autowired
+	SurveyRepository surveyRepository;
+
+	@Autowired
+	EntityManager entityManager;
+
+	@Test
+	@DisplayName("설문지 도메인 저장을 성공 한다.")
+	void saveSurvey_Success() {
+		// given
+		Survey instance = SurveyFixture.SURVEY.getInstance();
+
+		// when
+		Survey save = surveyRepository.save(instance);
+
+		// then
+		assertThat(save.getId()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("설문지에 질문을 추가하면 질문 정보가 함께 저장되고 함께 조회된다.")
+	void save_and_select_Question_With_Survey() {
+		// given
+		Survey survey = SurveyFixture.SURVEY.getInstance();
+		Question question = QuestionFixture.PARAGRAPH.getInstance();
+		survey.createQuestion(question);
+
+		// when
+		Survey saved = surveyRepository.save(survey);
+
+		// then
+		assertThat(saved.getQuestions().size()).isEqualTo(1);
+	}
+}
